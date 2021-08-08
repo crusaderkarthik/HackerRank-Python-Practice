3107b9ae69556356d693e889de46ff2bf2dccd77
Karthik K <karthikkanumolu123@gmail.com>
2021-08-09 03:42:19 +0530

    09/Aug/21

---
+++ find the runner up score.py
@@ -0,0 +1,8 @@
+N=int(input())
+list=list(set(map(int,input().split(" "))))
+list.sort(reverse=True)
+print (list[1])
+
+
+
+##https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
